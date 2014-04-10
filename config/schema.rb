a4fb63d11d7301437751be0e39f8432823e9134e
tett23 <tett23@gmail.com>
2014-04-10 23:16:37 +0900

    データベース関連のジョブを分離

--- config/schema.rb
+++ config/schema.rb
@@ -1,5 +1,5 @@
 ActiveRecord::Schema.define(version: 0) do
-  create_table :jobs do |t|
+  create_table :jobs, force: true do |t|
     t.integer :type, null: false
     t.datetime :updated_at
     t.datetime :created_at
--- lib/job.rb
+++ lib/job.rb
@@ -2,11 +2,8 @@ class Job
   def initialize(env=:development)
     @env = env
     establish_db_connection()
-    update_schema()
   end
-  def type
-    @type ||= :undefined
-  end
+  attr_reader :env, :database_config
 
   def execute!
   end
@@ -14,31 +11,11 @@ class Job
   private
   def establish_db_connection
     @database_config = YAML.load_file(database_config_path).symbolize_keys[@env].symbolize_keys
-    ActiveRecord::Base.establish_connection(
-      adapter: @database_config[:adapter],
-      encoding: @database_config[:encoding],
-      username: @database_config[:username],
-      password: @database_config[:password],
-      host: @database_config[:host]
-    )
-    ActiveRecord::Base.connection.create_database(@database_config[:database]) rescue nil
     ActiveRecord::Base.establish_connection(@database_config)
   end
 
   def database_config_path
     base = File.expand_path(__FILE__ + '/../../')
     File.expand_path(File.join(base, 'config/database.yml'))
-  end
-
-  def database_schema_path
-    base = File.expand_path(__FILE__ + '/../../')
-    File.expand_path(File.join(base, 'config/schema.rb'))
-  end
-
-  def update_schema
-    load database_schema_path
-  end
-
-  def database_config
   end
 end
