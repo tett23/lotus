c6c8305124c2dc2c7717d58b9427d3dde7e083f8
tett23 <tett23@gmail.com>
2014-04-14 07:53:41 -0700

    設定の読み込み

--- lib/lotus.rb
+++ lib/lotus.rb
@@ -1,12 +1,11 @@
 module Lotus
-  def config
-    config_path
-    #@config = YAML.load_file()
+  def self.config
+    @config = YAML.load_file(config_path).symbolize_keys
   end
 
   private
-  def config_path
+  def self.config_path
     root = File.expand_path('../..', __FILE__)
-    p root
+    root + '/config/lotus.yml'
   end
 end
