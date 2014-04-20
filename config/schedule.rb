4eb86875c6862003237d81d6c79c99ec662df3f9
tett23 <tett23@gmail.com>
2014-04-20 09:24:24 +0900

    cron設定

---
+++ config/schedule.rb
@@ -0,0 +1,27 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
+
+set :output, "/home/tett23/projects/lotus/log/whenever.log"
+job_type :lotus, "cd :path > /dev/null && source /home/tett23/.rvm/environments/lotus && bin/lotus :task >> /home/tett23/projects/lotus/log/whenever.log 2>> /home/tett23/projects/lotus/log/whenever.error.log"
+
+every 1.minute do
+  lotus "process"
+end
